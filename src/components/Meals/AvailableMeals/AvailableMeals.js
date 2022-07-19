9f1b39c4133d74a99fa031dfc65ee390f263f3f2
brianmuturi2 <brianmuturi2@gmail.com>
2022-07-19 17:28:22 +0300

    feat(meals): created meals components

---
+++ src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -0,0 +1,17 @@
+import {DUMMY_MEALS} from '../dev-data/dummy-meals';
+import classes from './AvailableMeals.module.css';
+
+const AvailableMeals = () => {
+
+    const mealsList = DUMMY_MEALS.map(meal => <li key={meal.id}>{meal.name}</li>);
+
+    return (
+        <section className={classes.meals}>
+            <ul>
+                {mealsList}
+            </ul>
+        </section>
+    )
+};
+
+export default AvailableMeals;
