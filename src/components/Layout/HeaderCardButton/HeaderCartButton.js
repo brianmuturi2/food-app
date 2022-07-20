cd4a78b8163bf8f8c94bfd2bed4cdae206d1303c
brianmuturi2 <brianmuturi2@gmail.com>
2022-07-20 08:58:40 +0300

    feat(modal): added modal state

--- src/components/Layout/HeaderCardButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCardButton/HeaderCartButton.js
@@ -1,9 +1,9 @@
 import classes from './HeaderCartButton.module.css'
 import CartIcon from '../../Cart/CartIcon/CartIcon';
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
     return (
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.showCart}>
             <span className={classes.icon}><CartIcon/></span>
             <span>Your Cart</span>
             <span className={classes.badge}>3</span>
