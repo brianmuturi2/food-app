f88f0b5d1e83ecb7150052f1c617ff717c57c2e4
brianmuturi2 <brianmuturi2@gmail.com>
2022-07-21 14:17:59 +0300

    feat(cart): added cart state and reducer

--- src/Store/Cart/CartProvider.js
+++ src/Store/Cart/CartProvider.js
@@ -1,14 +1,38 @@
 import CartContext from './cart-context';
+import {useReducer} from 'react';
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD':
+            const updatedItems = state.items.concat(action.payload)
+            const updatedTotalAmount = state.totalAmount + action.payload.price * action.payload.amount
+            return {
+                items: updatedItems,
+                totalAmount: updatedTotalAmount
+            }
+    }
+    return defaultCartState
+}
 
 const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
 
-    const addItemToCartHandler = item => {};
+    const addItemToCartHandler = item => {
+        dispatchCartAction({type: 'ADD', payload: item})
+    };
 
-    const removeItemFromCartHandler = id => {};
+    const removeItemFromCartHandler = id => {
+        dispatchCartAction({type: 'REMOVE', payload: id})
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     }
