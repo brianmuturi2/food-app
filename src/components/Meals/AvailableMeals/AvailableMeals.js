c66f6fdf71414170cf691f7ef9fd0b7e9e0aa703
brianmuturi2 <brianmuturi2@gmail.com>
2022-07-24 22:43:07 +0300

    feat(meals): fetched meals from firebase

--- src/components/Meals/AvailableMeals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -2,10 +2,38 @@ import {DUMMY_MEALS} from '../dev-data/dummy-meals';
 import classes from './AvailableMeals.module.css';
 import Card from '../../Shared/Card/Card';
 import MealItem from '../MealItem/MealItem';
+import {useEffect, useState} from 'react';
 
 const AvailableMeals = () => {
 
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
+    const [meals, setMeals] = useState({
+        meals: []
+    })
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://react-meals-2a831-default-rtdb.firebaseio.com/meals.json')
+            console.log('res is ', response)
+            const responseData = await response.json()
+
+            const meals = [];
+
+            for (const key in responseData) {
+                meals.push({
+                    id: key,
+                    name: responseData[key]['name'],
+                    description: responseData[key]['description'],
+                    price: responseData[key]['price'],
+                })
+            }
+
+            setMeals(prevState => ({...prevState, meals: [...meals]}))
+        }
+
+        fetchMeals()
+    }, [])
+
+    const mealsList = meals.meals.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
 
     return (
         <section className={classes.meals}>
